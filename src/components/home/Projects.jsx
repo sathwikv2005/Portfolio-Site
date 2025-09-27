cf44b33d0d9aaf275652190d50b73c8ab26eefac
sathwikv2005 <sathwikv2005@gmail.com>
2025-09-27 18:03:50 +0530

    update projects list

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -1,11 +1,62 @@
 import { FaGithub, FaHtml5, FaJava, FaNodeJs, FaAndroid } from 'react-icons/fa6'
-import { FaReact, FaJsSquare, FaGlobe } from 'react-icons/fa'
+import { FaReact, FaJsSquare, FaGlobe, FaApple, FaGlobeAmericas } from 'react-icons/fa'
 import { SiExpress, SiMongodb, SiSocketdotio } from 'react-icons/si'
 
 export default function Projects() {
 	return (
-		<div className="home--project--wrapper">
+		<div id="projects" className="home--project--wrapper">
 			<div className="project--heading">Projects</div>
+			<div className="home--project">
+				<div className="home--project--details">
+					<a target="blank" href="https://github.com/sathwikv2005/KitsuneAndroidApp">
+						<div className="project--title hover">Better VTOP</div>
+					</a>
+					<div className="project--des">
+						BetterVTOP is a React Native app that enhances the VITAP's student portal (VTOP)
+						experience by providing a faster, cleaner, and more user-friendly interface. It
+						integrates core features like attendance tracking, exam schedules, and file downloads
+						while ensuring smooth navigation and an optimized mobile-first design.
+					</div>
+					<div className="project--techstack home--techstack">
+						<div className="home--skill hover shinny">
+							<FaReact color="#61DBFB" className="home--skill--icon" />
+							React-Native
+						</div>
+						<div className="home--skill hover shinny">
+							<FaNodeJs color="#38783b" className="home--skill--icon" />
+							Nodejs
+						</div>
+						<div className="home--skill hover shinny">
+							<FaAndroid color="#38783b" className="home--skill--icon" />
+							Android
+						</div>
+						<div className="home--skill hover shinny">
+							<FaApple color="#ffffff" className="home--skill--icon" />
+							iOS
+						</div>
+						<div className="home--skill hover shinny">
+							<FaGlobeAmericas color="#ffffff" className="home--skill--icon" />
+							Web Scraping
+						</div>
+					</div>
+					<div className="project--btns">
+						<a target="blank" href="https://github.com/sathwikv2005/BetterVTOP-VITAP">
+							<FaGithub className="project--btn hover" />
+						</a>
+					</div>
+				</div>
+				<img
+					src="/betterVTOP.png"
+					onClick={() => {
+						window.open(
+							'https://github.com/sathwikv2005/BetterVTOP-VITAP',
+							'_blank',
+							'noopener,noreferrer'
+						)
+					}}
+					className="home--project--img hover"
+				/>
+			</div>
 			<div className="home--project">
 				<div className="home--project--details">
 					<a target="blank" href="https://smallurl.sathwik.xyz ">
@@ -112,33 +163,6 @@ export default function Projects() {
 					}}
 					className="home--project--img hover"
 				/>
-			</div>
-			<div className="home--project">
-				<div className="home--project--details">
-					<a target="blank" href="https://github.com/sathwikv2005/KitsuneAndroidApp">
-						<div className="project--title hover">Kitsune Android APP</div>
-					</a>
-					<div className="project--des">
-						A smart class reminder app built using Java and Android Studio. Kitsune notifies you
-						about upcoming classes, tracks your attendance, and calculates how many more classes you
-						need to attend to meet the minimum criteria.
-					</div>
-					<div className="project--techstack home--techstack">
-						<div className="home--skill hover shinny">
-							<FaJava color="#f45a3c" className="home--skill--icon" />
-							Java
-						</div>
-						<div className="home--skill hover shinny">
-							<FaAndroid color="#38783b" className="home--skill--icon" />
-							Android
-						</div>
-					</div>
-					<div className="project--btns">
-						<a target="blank" href="https://github.com/sathwikv2005/KitsuneAndroidApp">
-							<FaGithub className="project--btn hover" />
-						</a>
-					</div>
-				</div>
 			</div>
 		</div>
 	)
